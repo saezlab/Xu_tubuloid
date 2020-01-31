a620565a4828dc435196ebf5ca4516d62730c937
jperales <jperales.bioinfo@gmail.com>
2020-01-31 17:27:23 +0100

    Update make

--- make.sh
+++ make.sh
@@ -1,10 +1,35 @@
+### TITLE : Make file for reproducible results
+### AUTHOR : Javier Perales-Paton - javier.perales@bioquant.uni-heidelberg.de
+
+## Please note: this script is supposed to be run in the root folder of the repository
+
+# Store the root folder of the project
+echo -e "[INFO] : Store current working directory as root of the project.";
+PWD_project="$PWD"
+
 # Define $PATH for generating the Rmarkdown document output
 export RSTUDIO_PANDOC=/usr/lib/rstudio/bin/pandoc
-# Generate reports for individual analysis for each sample
-R -e "rmarkdown::render('CK121_CD24_individual_analysis.Rmd',output_file='CK121_CD24_individual_analysis.html')"
-R -e "rmarkdown::render('CK120_CD13_individual_analysis.Rmd',output_file='CK120_CD13_individual_analysis.html')"
-R -e "rmarkdown::render('CK119_organoid_individual_analysis.Rmd',output_file='CK119_organoid_individual_analysis.html')"
-R -e "rmarkdown::render('CK5_organoid_individual_analysis.Rmd',output_file='CK5_organoid_individual_analysis.html')"
-R -e "rmarkdown::render(input='CK120_CK121_GO.Rmd',output_file='CK120_CK121_GO.pdf')"
-# Generate main figures with a merged analysis (not integrated, but the four samples in same color scheme and scale)
-R -e "rmarkdown::render('merged_analysis.Rmd',output_file='merged_analysis.html')"
+
+# Get all bash scripts within the project to be run one by one
+echo -e "[INFO] : Finding bash scripts to build markdowns for reproducible analysis.";
+runs=`find $PWD_project/Individual_analysis_* -name run_rmd.sh`;
+
+# Run one by one all the 
+echo -e "[INFO] : Running individual analysis...";
+for run_sh in "${runs[@]}";do
+	echo -e "[INFO] \t Starting with '$(dirname $(basename $run_sh))' at $(date)";
+	cd $(dirname $run_sh);
+	bash $(basename $run_sh);
+	cd $PWD_project;
+done
+
+# Finally run the merged analysis
+echo -e "[INFO] : Creating figures for manuscript in a merged analysis with the four samples.";
+cd Merged_analysis;
+bash run_rmd.sh
+
+# Completed
+echo -e "[INFO] : Finished! ";
+echo -e "[INFO] : \t You can find reports of every step in the analysis in every *.md genereted file.";
+echo -e "[INFO] : \t Final figures are included at './Merged_analysis/output/figs'";
+
